9e7aabbf9f1311aa63a063a2911fc8f8a786fae7
Igor Karatayev <igor.cwer@gmail.com>
2025-06-06 02:20:50 +0300

    testing

--- core/core_test.go
+++ core/core_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/tools/go/packages"
 )
 
 func TestUnmarshalTag(t *testing.T) {
@@ -38,4 +39,27 @@ func TestUnmarshalTag(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, len(tag), 2)
 	}
+}
+
+func TestPkgAboveTag(t *testing.T) {
+	p1 := Package{
+		Pkg: &packages.Package{
+			PkgPath: "github.com/igadmg/gogen/core",
+		},
+	}
+	p2 := Package{
+		Pkg: &packages.Package{
+			PkgPath: "github.com/igadmg/gogen/core/gfx",
+		},
+	}
+	p3 := Package{
+		Pkg: &packages.Package{
+			PkgPath: "github.com/igadmg/gogen/gridcity",
+		},
+	}
+
+	assert.True(t, p1.Above(&p2))
+	assert.False(t, p2.Above(&p1))
+	assert.True(t, !p3.Above(&p1))
+	assert.True(t, !p1.Above(&p3))
 }
